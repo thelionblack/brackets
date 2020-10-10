571beff549db233662c24dcceee61775a10ab76f
Maksim Storazhau <Thelionblack.com@gmail.com>
2020-10-10 19:42:16 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let rightSequens = '';
+  let brackets = {};
+
+  bracketsConfig.forEach(elem => {
+    brackets[elem[0]] = elem[1];
+  });
+  str.split('').forEach(elem => {
+    if (brackets.hasOwnProperty(elem)) {
+      if(rightSequens.slice(-1) === elem && brackets.hasOwnProperty(brackets[elem])) {
+        rightSequens = rightSequens.slice(0, -1);
+      } else {
+        rightSequens += elem;
+      }
+    } else {
+      if (brackets[rightSequens.slice(-1)] !== elem || rightSequens.length === 0) {
+        rightSequens += 'asdsdsa';
+      }
+      rightSequens = rightSequens.slice(0, -1);
+    }
+  });
+  return !Boolean(rightSequens);
+};
